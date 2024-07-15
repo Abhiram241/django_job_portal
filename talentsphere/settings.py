bf6eb0ed2a3725a26e21564580bc4df54595ab38
Abhiram <81345491+Abhiram241@users.noreply.github.com>
2024-07-15 18:54:42 +0530

    Added applied button status

--- talentsphere/settings.py
+++ talentsphere/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e_@mwghnn=^-wen%uph37-l)#-#psd4tfs9&w@se%_#bo0(c-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.4','127.0.0.1','192.168.29.235']
+ALLOWED_HOSTS = ['192.168.1.4','127.0.0.1','192.168.29.235',"192.168.46.223"]
 
 
 # Application definition
